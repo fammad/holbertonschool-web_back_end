d9fbb06d99a2dce518d4ccb1844f63e31f50eade
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-05-05 21:33:56 +0400

    Updated 0.add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,7 +2,7 @@
 '''Variable Annottations Task 0'''
 
 
-def add(a:float, b:float)-> float:
+def add(a: float, b: float) -> float:
     '''Add two floats number and returns float'''
-    return a+b
+    return a + b
 
