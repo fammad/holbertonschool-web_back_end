6f53be780eb0ad682444b04ddbe037bbd7c13cbd
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-05-05 22:00:16 +0400

    Added task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Variable Annotation'''
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    '''Imput list and return as float'''
+    return sum(imput_list)
