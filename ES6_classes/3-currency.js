4032c7e639277621aec0214facc8583c5eed9c9b
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-05-19 03:02:14 +0400

    ADDED N3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,23 @@
+export default class Currency {
+  constructor(code, name) {
+  if (typeof code !== 'string') throw TypeError("Code must be string");
+  this._code = code;
+  if (typeof name !== 'string') throw TypeError("Name must be a string");
+  this._name = name;
+  }
+
+  get code() {
+    return this._code
+  }
+  set code(newCode) {
+    if (typeof newCode !== 'string') throw TypeError("Code must be string");
+    this._code = newCode;
+  }
+  get name() {
+    return this._name
+  }
+  set name(newName) {
+    if (typeof newName !== 'string') throw TypeError("Code must be string");
+    this._code = newName;
+  }
+}
