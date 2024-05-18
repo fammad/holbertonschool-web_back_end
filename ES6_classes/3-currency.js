982beb3ff020e107d8d3fc6882b07fd188897c26
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-05-19 03:18:52 +0400

    ADDED N3

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,25 +1,29 @@
 export default class Currency {
   constructor(code, name) {
-  if (typeof code !== 'string') throw TypeError("Code must be string");
-  this._code = code;
-  if (typeof name !== 'string') throw TypeError("Name must be a string");
-  this._name = name;
-  }
+    if (typeof code !== 'string') throw TypeError('Code must be string');
+    this._code = code;
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    this._name = name;
+    }
 
   get code() {
-    return this._code
+    return this._code;
   }
+
   set code(newCode) {
-    if (typeof newCode !== 'string') throw TypeError("Code must be string");
+    if (typeof newCode !== 'string') throw TypeError('Code must be string');
     this._code = newCode;
   }
+
   get name() {
     return this._name
   }
+
   set name(newName) {
-    if (typeof newName !== 'string') throw TypeError("Code must be string");
+    if (typeof newName !== 'string') throw TypeError('Name must be string');
     this._code = newName;
   }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
